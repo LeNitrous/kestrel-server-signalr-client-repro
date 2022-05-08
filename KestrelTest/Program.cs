7071b2a3ef9cc8a85ba433892bc01241ec2da42d
Nitrous <nathan.alo2000@gmail.com>
2022-05-08 21:32:49 +0800

    create repro repo

---
+++ KestrelTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+var host = new WebHostBuilder()
+    .UseUrls("http://localhost:80")
+    .UseKestrel()
+    .Configure(app =>
+    {
+        app.Run(async (context) =>
+        {
+            await context.Response.WriteAsync("Hello World");
+        });
+    })
+    .Build();
+
+host.Run();
